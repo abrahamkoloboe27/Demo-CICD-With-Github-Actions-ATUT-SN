0633d0178b76198d9104ea4563a114bbb98a70d8
Abraham KOLOBOE <abrahamkoloboe2000@gmail.com>
2024-11-30 00:17:08 +0100

    Remove Mkdocs Build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   server: {
     port: 3000
   },
   build: {
     sourcemap: true,
-    minify: 'terser'
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
   }
 })
